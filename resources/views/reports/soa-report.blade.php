6a63b4bc9e8c4b7a105b5735cbbc14d351a46912
Lokesh <sharmalk1991@gmail.com>
2023-07-03 22:40:31 +0530

    Change SOA report

--- resources/views/reports/soa-report.blade.php
+++ resources/views/reports/soa-report.blade.php
@@ -137,34 +137,40 @@
 						- <b>Zone :</b> {{$zone->name}}
 					
 					@endif
-					
-					</td>
-					
 					@php
 					$totalPerson = $record->adult + $record->child;
 					$transferCostPerPersonSIC = 0;
 					$transferCostPerPersonPVT = 0;
-					if($record->transfer_option == "Shared Transfer"){
+					@endphp
+					</td>
+					
+					@if($record->transfer_option == "Shared Transfer")
+					@php
 					$markup_sic_transfer =  (($record->zonevalprice_without_markup) * ($record->markup_p_sic_transfer/100));
 					$transferCostPerPersonSIC = ($record->zonevalprice_without_markup + $markup_sic_transfer)/$totalPerson; 
-					}
-					if($record->transfer_option == 'Pvt Transfer')
-					{
-						$transferCostPerPersonPVT = $record->pvt_traf_val_with_markup/$totalPerson;
-					}
+					@endphp
+					{{$record->zonevalprice_without_markup + $markup_sic_transfer}} /{{$transferCostPerPersonSIC}}
+					@endif
+					@if($record->transfer_option == 'Pvt Transfer')
+						@php
+					$transferCostPerPersonPVT = $record->pvt_traf_val_with_markup/$totalPerson;
+					@endphp					
+					{{$record->pvt_traf_val_with_markup}}
 					
+					{{$transferCostPerPersonPVT}} 
+					@endif
+					
+                    <td>{{$record->adult}}</td>
+					<td>{{$record->child}}</td>
+					<td>
+					@php
 					$totalAdultPrice = $record->adultPrice + $transferCostPerPersonSIC + $transferCostPerPersonPVT;
 					$totalChildPrice = $record->child + $transferCostPerPersonSIC + $transferCostPerPersonPVT;
 					$vatAd = ((5/100)+$totalAdultPrice);
 					$vatCh = ((5/100)+$totalChildPrice);;
 					$totalAdultPriceWithVat = $totalAdultPrice + $vatAd;
 					$totalChildPriceWithVat = $totalChildPrice + $vatCh;
-					
-					@endphp
-					
-                    <td>{{$record->adult}}</td>
-					<td>{{$record->child}}</td>
-					<td>
+					@endphp	
 					{{number_format($totalAdultPriceWithVat,2)}}</td>
 					<td>{{($record->child > 0)?number_format($totalChildPriceWithVat,2):0}}</td>
 					<td>{{$record->discountPrice}}</td>
