f6adcb39cc4a74f7e6f21d2c2736b337f8c73f74
Lokesh <sharmalk1991@gmail.com>
2023-10-11 23:29:55 +0530

    Sds

--- resources/views/reports/voucher-activity-report.blade.php
+++ resources/views/reports/voucher-activity-report.blade.php
@@ -6,12 +6,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Ticket Only Report</h1>
+            <h1>Voucher Activity Report</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
-              <li class="breadcrumb-item active">Ticket Only Report</li>
+              <li class="breadcrumb-item active">Voucher Activity Report</li>
             </ol>
           </div>
         </div>
