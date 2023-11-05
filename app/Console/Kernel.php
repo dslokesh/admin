a0b96f62ec1d968269b6d520d180c21aa5a5adfd
Lokesh <sharmalk1991@gmail.com>
2023-11-05 23:23:55 +0530

    sdsd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
        $schedule->command('ticketAutoGenerate')->everyMinute();
+	   $schedule->command('supplierActualTotalCostAutoUpdate')->dailyAt('0:01');
     }
 
     /**
