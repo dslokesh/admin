ae13697772117eb5dd4bead96cf1c6a78081b8d5
Lokesh <sharmalk1991@gmail.com>
2023-12-26 09:54:33 +0530

    sdsd

--- app/Console/Commands/TicketAutoGenerate.php
+++ app/Console/Commands/TicketAutoGenerate.php
@@ -59,63 +59,66 @@ class TicketAutoGenerate extends Command
 		$child = $voucherActivity->child;
 		$totalTicketNeed = $adult+$child;
 		$countTotalTicketNeed = $totalTicketNeed;
-		$ticketQuery = Ticket::where('ticket_generated','0')->where('activity_id',$voucherActivity->activity_id)->where('activity_variant',$voucherActivity->variant_unique_code)->whereDate('valid_from', '<=',$voucherActivity->tour_date)->whereDate('valid_till', '>=',$voucherActivity->tour_date)->whereDate('tour_date', '>=',date("Y-m-d"))->orderBy("valid_till","ASC");
+		$countTotalTicketNeed = $totalTicketNeed;
+		//$ticketQuery = Ticket::where('ticket_generated','0')->where('activity_id',$voucherActivity->activity_id)->where('activity_variant',$voucherActivity->variant_unique_code)->whereDate('valid_from', '<=',$voucherActivity->tour_date)->whereDate('valid_till', '>=',$voucherActivity->tour_date)->whereDate('tour_date', '>=',date("Y-m-d"))->orderBy("valid_till","ASC");
+		
+		$ticketQuery = Ticket::where('ticket_generated','0')->where('activity_id',$voucherActivity->activity_id)->where('activity_variant',$voucherActivity->variant_unique_code)->whereDate('valid_from', '<=',$voucherActivity->tour_date)->whereDate('valid_till', '>=',$voucherActivity->tour_date)->orderBy("valid_till","ASC");
 		
 		$totalTickets =$ticketQuery->get();
 		$totalTicketCount =$totalTickets->count();
 		$tcArray = [];
 		if($totalTicketCount >= $totalTicketNeed)
 		{
-			
 			foreach($totalTickets as $ticket){
-				
 				if(($ticket->ticket_for == 'Adult') && ($adult > 0)){
+					$tcArray[$ticket->id] = $ticket->id;
+					$adult--;
+					$totalTicketNeed--;
+				} elseif(($ticket->ticket_for == 'Child') && ($child > 0)){
+					$tcArray[$ticket->id] = $ticket->id;
+					$child--;
+					$totalTicketNeed--;
+				} elseif(($ticket->ticket_for == 'Both') && ($totalTicketNeed > 0)){
+					// Check if the 'Both' ticket is for an adult or child
+					if ($adult > 0) {
 						$tcArray[$ticket->id] = $ticket->id;
 						$adult--;
-						$totalTicketNeed--;
-						
-				}
-				if(($ticket->ticket_for == 'Child') && ($child > 0)){
-					
+					} elseif ($child > 0) {
 						$tcArray[$ticket->id] = $ticket->id;
 						$child--;
-						$totalTicketNeed--;
-					
-				}if(($ticket->ticket_for == 'Both') && ($totalTicketNeed > 0)){
-					
-						$tcArray[$ticket->id] = $ticket->id;
-						$totalTicketNeed--;
+					}
+					$totalTicketNeed--;
 				}
-				
-				
 			}
+			
 			
 			if(($totalTicketNeed == 0) && (count($tcArray) == $countTotalTicketNeed)){
 				$tcCountEx = Ticket::where("voucher_id",'=',$voucherActivity->voucher_id)->where("voucher_activity_id",'=',$voucherActivity->id)->count();
-				if($tcCountEx > 0){
-				return redirect()->route('ticket.dwnload',$voucherActivity->id);	
-				} else {
+				if($tcCountEx == 0){
 				foreach($tcArray as $ta){
-					$tc = Ticket::find($ta);
-					$tc->voucher_activity_id = $voucherActivity->id;
-					$tc->ticket_generated = 1;
-					$tc->ticket_generated_by = Auth::user()->id;
-					$tc->generated_time = date("d-m-Y h:i:s");
-					$tc->voucher_id = $voucherActivity->voucher_id;
-					$tc->save();
+					$voucherCheck = Voucher::find($voucherActivity->voucher_id);
+					if($voucherCheck->booking_date >= date("Y-m-d")){
+						$tc = Ticket::find($ta);
+						$tc->voucher_activity_id = $voucherActivity->id;
+						$tc->ticket_generated = 1;
+						$tc->ticket_generated_by = Auth::user()->id;
+						$tc->generated_time = date("d-m-Y h:i:s");
+						$tc->voucher_id = $voucherActivity->voucher_id;
+						$tc->save();
+					}
 				}
 				
-				$voucher = Voucher::find($voucherActivity->voucher_id);
-				$agentsupplierId = '947d43d9-c999-446c-a841-a1aee22c7257';
-				$voucher = Voucher::find($voucherActivity->voucher_id);
-				$priceCal = SiteHelpers::getActivityPriceSaveInVoucherActivity("Ticket Only",$voucherActivity->activity_id,$agentsupplierId,$voucher,$voucherActivity->variant_unique_code,$voucherActivity->adult,$voucherActivity->child,$voucherActivity->infant,$voucherActivity->discount);
-				$totalprice = $priceCal['totalprice'];
+				//$voucher = Voucher::find($voucherActivity->voucher_id);
+				//$agentsupplierId = '947d43d9-c999-446c-a841-a1aee22c7257';
+				//$voucher = Voucher::find($voucherActivity->voucher_id);
+				//$priceCal = SiteHelpers::getActivityPriceSaveInVoucherActivity("Ticket Only",$voucherActivity->activity_id,$agentsupplierId,$voucher,$voucherActivity->variant_unique_code,$voucherActivity->adult,$voucherActivity->child,$voucherActivity->infant,$voucherActivity->discount);
+				//$totalprice = $priceCal['totalprice'];
 				
-				$voucherActivity->ticket_generated = 1;
-				$voucherActivity->supplier_ticket = $agentsupplierId;
-				$voucherActivity->actual_total_cost = $totalprice;
-				$voucherActivity->status = 4;
-				$voucherActivity->save();
+				//$voucherActivity->ticket_generated = 1;
+				//$voucherActivity->supplier_ticket = $agentsupplierId;
+				//$voucherActivity->actual_total_cost = $totalprice;
+				//$voucherActivity->status = 4;
+				//$voucherActivity->save();
 				}
 			}
 			
